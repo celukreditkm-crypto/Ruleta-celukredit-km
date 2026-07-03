c29f528651d681b3c0baa80a406eb2cb773b8d21
celukreditkm-crypto <celukredit.km@gmail.com>
2026-07-03 04:59:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+const premios = [
+"⚽ Balón del Mundial",
+"👕 Camisa de Colombia",
+"🧢 Gorra de Colombia",
+"📱 Forro Siliconado",
+"💎 Vidrio Premium"
+];
+
+const formulario=document.getElementById("formulario");
+const ruleta=document.getElementById("ruleta");
+const wheel=document.getElementById("wheel");
+const boton=document.getElementById("girar");
+const resultado=document.getElementById("resultado");
+
+formulario.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+formulario.style.display="none";
+
+ruleta.style.display="block";
+
+});
+
+boton.addEventListener("click",()=>{
+
+const premio=Math.floor(Math.random()*premios.length);
+
+const giro=360*6+(premio*72);
+
+wheel.style.transform=`rotate(${giro}deg)`;
+
+setTimeout(()=>{
+
+resultado.innerHTML="🎉 Ganaste: <br><b>"+premios[premio]+"</b>";
+
+},5000);
+
+});
