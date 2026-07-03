cc23b1930b8b26c61085731df5a93c53ef1110de
celukreditkm-crypto <celukredit.km@gmail.com>
2026-07-03 04:51:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const formulario = document.getElementById("formulario");
 
+  formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nombre = formulario.querySelector('input[type="text"]').value.trim();
+    const cedula = formulario.querySelector('input[type="number"]').value.trim();
+    const acepta = formulario.querySelector('input[type="checkbox"]').checked;
+
+    if (nombre === "" || cedula === "") {
+      alert("Por favor completa todos los campos.");
+      return;
+    }
+
+    if (!acepta) {
+      alert("Debes aceptar el tratamiento de datos.");
+      return;
+    }
+
+    alert("¡Datos enviados correctamente! Ahora iniciará la ruleta.");
+
+    // Aquí después agregaremos la ruleta.
+  });
+});
